d1b5891a8a4cd710a68190a0c116904c1072dd9c
alvarorichard <alvaro10048@protonmail.com>
2023-08-05 22:12:18 -0300

    function_body

--- src/main.c
+++ src/main.c
@@ -37,11 +37,16 @@ enum {
   Assign, Cond, Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt, Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec, Brak
 };
 
+enum { CHAR , PTR }; // Adicione outros valores conforme necessário
+
 
 
 
 int token;
 int token_val;
+
+int basetype;
+int index_of_bp;
 
 char *src = NULL;
 
@@ -309,6 +314,68 @@ void match(int tk) {
 
     next();
 }
+void function_body() {
+    // type func_name (...) {...}
+    //                   -->|   |<--
+
+    // ... {
+    // 1. local declarations
+    // 2. statements
+    // }
+
+    int pos_local; // position of local variables on the stack.
+    int type;
+    pos_local = index_of_bp;
+
+    while (token == Int || token == Char) {
+        // local variable declaration, just like global ones.
+        basetype = (token == Int) ? INT : CHAR;
+        match(token);
+
+        while (token != ';') {
+            type = basetype;
+            while (token == Mul) {
+                match(Mul);
+                type = type + PTR;
+            }
+
+            if (token != Id) {
+                // invalid declaration
+                printf("%d: bad local declaration\n", line);
+                exit(-1);
+            }
+            if (current_id[Class] == Loc) {
+                // identifier exists
+                printf("%d: duplicate local declaration\n", line);
+                exit(-1);
+            }
+            match(Id);
+
+            // store the local variable
+            current_id[BClass] = current_id[Class]; current_id[Class]  = Loc;
+            current_id[BType]  = current_id[Type];  current_id[Type]   = type;
+            current_id[BValue] = current_id[Value]; current_id[Value]  = ++pos_local;   // index of current parameter
+
+            if (token == ',') {
+                match(',');
+            }
+        }
+        match(';');
+    }
+
+    // save the stack size for local variables
+    *++text = ENT;
+    *++text = pos_local - index_of_bp;
+
+    // statements
+    while (token != '}') {
+        statement();
+    }
+
+    // emit code for leaving the sub function
+    *++text = LEV;
+}
+
 
 void function_declaration() {
     
@@ -331,7 +398,7 @@ void function_declaration() {
 }
 
 
-enum { CHAR,  PTR };
+
 
 void enum_declaration() {
    
